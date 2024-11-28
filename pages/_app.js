9b1aeb1a93c184308e39d589e240fa77e6ec75db
Joseph Wambua <josefwambua2@gmail.com>
2024-11-28 20:53:04 +0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+
+import { Poppins } from 'next/font/google';
+
+// Initialize Poppins font
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: '600',
+});
+
+export default function App({ Component, pageProps : { session, ...pageProps } }) {
+  return (
+    <main className={poppins.className}>
+      <SessionProvider session={session}>
+      <Component {...pageProps} />
+      </SessionProvider>
+    </main>
+  );
 }
